019a5e1ec3fb687dd8a29d5e4d2c2819e8547379
cldblood <144237883+cldblood@users.noreply.github.com>
2023-10-26 14:16:55 +0300

    Add files via upload

---
+++ kr1.pas
@@ -0,0 +1,30 @@
+﻿program zad_kr1;  
+
+var x, y: real;
+    count: integer;
+    const e = 2.71;
+  
+begin
+  x:= -9;
+  count:= 1;
+  
+  while x <= 5 do
+  begin  
+  if x < -7 then y:= Power(e, x) * Power(x, 2) - 81
+  else
+  
+  if (-7 <= x) and (x < -3) then y:= sqr(x) * (-x) - power(x, 3)
+  else
+  
+  if (-3 <= x) and (x < 3) then y:= (power(x, 3) / cos(x)) * (sqr(x) / sin(x))
+  else
+  
+  if (3 <= x) then y:= x / 70 - -x/cos(x);
+
+  writeln(x:1:1, ' = ', y:0:2, ' ', '(', count, ')');
+  count:= count + 1;
+  x:= x + 0.1;
+  end;
+end.
+ 
+
